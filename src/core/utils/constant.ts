e58443f51b7ae2ab810aedfc6bd27399b4dd3f71
Mohit Rai <58872582+mohitsrai02@users.noreply.github.com>
2021-08-12 01:41:06 +0530

    Update constant.ts

--- src/core/utils/constant.ts
+++ src/core/utils/constant.ts
@@ -1,16 +1,15 @@
-
 export class Constants {
   public static GENERAL = {
     GA_TRACKING_ID: "UA-150439015-2",
   };
   public static projectDashboard = [
     {
-      projectName: "Network Dashboard",
-      iframeURL:"https://analytics.dapplooker.com/public/dashboard/c70b56bd-09a0-4472-a557-796afdc64d3b",
+      projectName:"Network Dashboard",
+      iframeURL:"https://analytics.dapplooker.com/public/dashboard/b40e031c-42d1-43e2-84ea-97f07a149a47",
     },
     {
-      projectName: "Storage Dashboard",
-      iframeURL:"https://analytics.dapplooker.com/public/dashboard/b40e031c-42d1-43e2-84ea-97f07a149a47",
+      projectName:"Provider Performance",
+      iframeURL:"https://analytics.dapplooker.com/public/dashboard/aae19f0c-5265-4de4-b691-cc19f32465f8"
     },
   ];
   public static URLs = {
