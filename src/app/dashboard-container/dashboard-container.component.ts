d5c1085fc4474b455b237b0a2ec70f09fb348f3b
Mohit Rai <mohitsrai@live.in>
2021-08-17 13:50:39 +0530

    2nd tab will load on click

--- src/app/dashboard-container/dashboard-container.component.ts
+++ src/app/dashboard-container/dashboard-container.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Constants } from 'src/core/utils/constant';
+import { number } from '../utility/svg-icon/svg-icon';
 @Component({
   selector: 'app-dashboard-container',
   templateUrl: './dashboard-container.component.html',
   styleUrls: ['./dashboard-container.component.scss']
 })
-
 export class DashboardContainerComponent implements OnInit {
   dashboardList = Constants.projectDashboard;
   constructor() { }
-
+  isLoaded: boolean[] = [true,false,false,false];
+    loadIframe(number: any){
+      this.isLoaded[number]=true;
+    }
   ngOnInit(): void {
-  }
 
+  }
 }
